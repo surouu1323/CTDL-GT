13c642a4ec3890f85ae8837c329c80bf95da7ab3
surouu1323 <nguyenvietthanh12345678910@gmail.com>
2024-01-30 09:49:45 +0700

    modified:   bai tap buoi 4/bai 3.py

--- bai tap buoi 4/bai 3.py
+++ bai tap buoi 4/bai 3.py
@@ -1,5 +1,11 @@
 
 
-numb = str(input("nhap so muon dao : "))
+numb = int(input("nhap so muon dao : "))
+res = 0
+
+while(numb != 0):
+    a = numb % 10
+    res = res * 10 + a
+    numb = numb // 10
      
-print(str(numb)[::-1])
+print(res)
