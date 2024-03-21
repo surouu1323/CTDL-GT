ba325e3282ec3849b2feb578bc2441a7dea38cce
surouu1323 <nguyenvietthanh12345678910@gmail.com>
2024-03-21 09:21:24 +0700

    modified:   buoi 9 thuc hanh/bai_5.py

--- buoi 9 thuc hanh/bai_5.py
+++ buoi 9 thuc hanh/bai_5.py
@@ -1,18 +1,27 @@
-def perfect(n):
-    numb = 0
-    for i in range(1, n):
-        if (n % i == 0):
-            numb = numb +i
-    if numb < n: 
-        print(n,"la so deficient")
-    else:
-        print(n,"la so perfect")
-
-
-n = int(input("Nhập số n: "))
-
-if n <= 0:
-    print("Nhập lại số nguyên dương.")
-else:
-    perfect(n)
+def perfect(x,y):
     
+    for n in range(x, y+1):
+        numb = int(0)
+        for i in range(1, n):
+            if (n % i == 0):
+                numb = numb + i        
+        if numb < n: 
+            print(n,"la so deficient")
+        elif numb > n:
+            print(n,"la so abundant")
+        else:
+            print(n,"la so perfect")
+
+
+x = int(input("Nhập số x: "))
+y = int(input("Nhập số y: "))
+
+if x <= 0:
+    print("Nhập lại x ")
+elif y <= 0:
+    print("Nhập lại x ")
+else:
+    perfect(x,y)
+    
+    
+print("Nhập vào số N cần tìm ước: ")
